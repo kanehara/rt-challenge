c4b7641e0acdfda4f586134bbd1df0df31facb04
yohei.kanehara <yohei.kanehara@vodori.com>
2017-08-13 03:02:26 -0500

    Basic info showing

--- client/src/containers/dashboard/Dashboard.js
+++ client/src/containers/dashboard/Dashboard.js
@@ -19,7 +19,12 @@ export const Dashboard = ({ projects, getProjects }) => {
         <div>
             <div>
                 { projects.map(project => (
-                    <h1 key={project.id}>{project.id}</h1>
+                    <div key={project.id}>
+                        <h1>{project.name}</h1>
+                        <h3>{project.project_type}</h3>
+                        <h3>{project.iteration_length} week{project.iteration_length > 1 ? 's' : ''}/iteration</h3>
+                        <h3>Iteration: {project.current_iteration_number}</h3>
+                    </div>
                 ))}
             </div>
         </div>
