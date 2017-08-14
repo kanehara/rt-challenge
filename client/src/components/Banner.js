c57ac69790ed2bc11087567075a8931ce37cb298
yohei.kanehara <yohei.kanehara@vodori.com>
2017-08-14 01:12:59 -0500

    Created banner

---
+++ client/src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './Banner.css'
+
+export default () => (
+    <div className="u--primaryBlueBackground banner">
+        <Link to="/">RT PivotalTracker</Link>
+    </div>
+)
--- client/src/components/projectcard/ProjectCard.js
+++ client/src/components/projectcard/ProjectCard.js
@@ -6,7 +6,7 @@ export default withRouter(({project, history}) => (
     <div className="project card" onClick={ () => history.push(`/projects/${project.id}/stories`) }>
         <h1>{project.name}</h1>
         <h3>{project.project_type}</h3>
-        <h3>{project.iteration_length} week{project.iteration_length > 1 ? 's' : ''}/iteration</h3>
+        <h3>{project.iteration_length} week{project.iteration_length > 1 ? 's' : ''} / iteration</h3>
         <h3>Iteration: {project.current_iteration_number}</h3>
     </div>
 ))
