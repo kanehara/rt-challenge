0a9a07de7522debc2d7100f4454dc6cceef38b88
yohei.kanehara <yohei.kanehara@vodori.com>
2017-08-14 02:51:26 -0500

    Added loading

--- client/src/containers/Stories.js
+++ client/src/containers/Stories.js
@@ -5,7 +5,9 @@ import * as actions from '../store/actions'
 import StoryBoard from '../components/stories/StoryBoard'
 
 const mapStateToProps = (state) => ({
-  stories: state.stories.stories
+  status: state.stories.status,
+  stories: state.stories.stories,
+  label: state.stories.label
 })
 
 const mapDispatchToProps = (dispatch) => ({
@@ -25,11 +27,29 @@ export class Stories extends React.Component {
   }
 
   render() {
-    return (
-        <div>
-          <StoryBoard stories={this.props.stories} />
-        </div>
-    )
+    switch(this.props.status) {
+        case 'loading':
+            return (
+                <div>
+                    <h3>Loading...</h3>
+                </div>
+            )
+        case 'failure':
+            return (
+                <div>
+                    <h3>Failed to retrieve stories</h3>
+                </div>
+            )
+        case 'success':
+            return (
+                <div>
+                    <h4>Showing: {this.props.label}</h4>
+                    <StoryBoard stories={this.props.stories} />
+                </div>
+            )
+        default:
+            return (<div><h3>500</h3></div>)
+    }
   }
 }
 
