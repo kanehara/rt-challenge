1869fdcc04918408181714b1eec10cbf8b818716
yohei.kanehara <yohei.kanehara@vodori.com>
2017-08-13 19:37:02 -0500

    Added margin to innerAppˆ

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -5,7 +5,7 @@ import {_404} from '../containers/error'
 import Stories from '../containers/Stories'
 
 export default (
-    <div>
+    <div className="innerApp">
         <Switch>
             <Route path="/dashboard" component={Dashboard}/>
             <Redirect exact from="/" to="/dashboard"/>
